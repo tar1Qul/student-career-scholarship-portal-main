fe9338442a792a5267ae6cc254fdfc8116f216fd
MD. TARIQUL ISLAM <tariqul.islam.cse@ulab.edu.bd>
2026-08-14 04:22:27 +0600

    Delete student-career-scholarship-portal-main-main directory

--- student-career-scholarship-portal-main-main/backend/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-session_start();
-$_SESSION = [];
-session_destroy();
-
-header('Location: ../login.html');
-exit;
-?>
--- student-career-scholarship-portal-main-main/backend/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'config.php';
-
-echo "PHP is working!<br>";
-echo "Database connected successfully!";
-
-?>
